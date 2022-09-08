71f16f6b76e79eb63694b3819793b471b28240f3
kurt <thekrjackson@gmail.com>
2022-09-08 10:15:04 -0500

    finished hw2 and quiz 2

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -5,72 +5,78 @@ Programmer: Kurt Jackson Z1875101
 
 Section: 02
 
-Date Due: 9/9/2022
+Date Due: 9/16/2022
 
-Purpose: To take user input of baseball game data, calculate batter statistics, and desplay the result to the user
+Purpose: To take user input of perabola coefficents, calculate vertex coordanates, and desplay the result to the user
 ***************************************************************/
 
 #include <iostream>
 #include <iomanip>
+#include <math.h>
 
-using namespace std; // using the standard namespace to gain functions from iostream
+using namespace std;
+
+
 
 
 int main() {
     
-    
-    // ---- declaring all the varables needed ----
-    int numAtBat;
-    int numOfHits;
-    int numDoubles;
-    int numTriples;
-    int numOfRuns;
-    int numOfSingles;
-    int numOfBases;
-    float battingAve;
-    float sluggingPercent;
-    // -------------------------------------------
+    // ---- declaring variables ----
+    int coefA = 0;
+    int coefB;
+    int coefC;
+    float X;
+    float Y;
+    float discriminant;
+    float root1, root2;
     
     
-    // ---- gathering user input -----------------
-    cout << "Enter the number of at bats: ";
-    cin >> numAtBat;
-    
-    cout << "Enter the number of hits: ";
-    cin >> numOfHits;
-    
-    cout << "Enter the number of doubles: ";
-    cin >> numDoubles;
-    
-    cout << "Enter the number of triples: ";
-    cin >> numTriples;
-    
-    cout << "Enter the number of home runs: ";
-    cin >> numOfRuns;
-    // -------------------------------------------
-    
-    
-    // ---- calculating statistics on user input -
-    numOfSingles = numOfHits - numDoubles - numTriples - numOfRuns;
-    numOfBases = numOfSingles + (numDoubles * 2) + (numTriples * 3) + (numOfRuns * 4);
-    battingAve = static_cast< float >(numOfHits) / static_cast< float >(numAtBat); // used static_cast to cased int as float
-    sluggingPercent = static_cast< float >(numOfBases) / static_cast< float >(numAtBat);
-    //--------------------------------------------
+    // ---- getting user input ----
+    cout << "Enter the \"a\" coefficient: ";
+    cin >> coefA;
+
 
     
-    // ---- displaying breakdown bases claimed ---
-    cout << endl << endl << "singles:" << setw(5) << numOfSingles << "   doubles:" << setw(5) <<
-    numDoubles <<  "   Triples:" << setw(5) <<  numTriples << "   Home Runs:" <<
-    setw(5) << numOfRuns << endl;
-    // -------------------------------------------
+    while (coefA == 0 ) {
+        cout << "Error: the a-coefficient MUST be non-zero. Try again: ";
+        cin >> coefA; // why does this line not get run when coefA !=== int?
+        if(cin.fail()) {cout << "ERROR: invalid input plese restart\n\n"; return 0;}
+    }
+    
+    cout << "Enter the \"b\" coefficient: ";
+    cin >> coefB;
+    
+    cout << "Enter the \"c\" coefficient: ";
+    cin >> coefC;
     
     
-    // ---- displaying batter stats --------------
-    cout << "\nTotal Bases:        "  << setw(9) << numOfBases << endl;
-    cout << "\nBatting Average:    "  << setw(9) << fixed << setprecision(3) << battingAve << endl;
-    cout << "\nSlugging Percentage:"  << setw(9) << fixed << setprecision(3) << sluggingPercent <<
-    endl << endl;
-    // -------------------------------------------
+    // ----------------------------
     
-    return 42; // because it's the answer to everything
+    // ---- math ----
+    
+    X = -1 * coefB / ( 2 * coefA);
+    Y = (coefA * X ^ 2) + (coefB * X) + coefC;
+    
+    discriminant = (coefB ^ 2) - (4 * coefA * coefC);
+    root1 = (-1 * coefB + sqrt( discriminant ));
+    root2 = (-1 * coefB - sqrt( discriminant ));
+    
+    // ----------------------------
+    
+    // ---- returning calulated data ----
+    
+    cout << "\n\nParabola:  " << coefA << "x^2 + " << coefB << "x + " << coefC << endl;
+    cout << "-------------------------------\nVertex\n";
+    cout << "X Coordinate             " << setprecision(4) << X << endl;
+    cout << "Y Coordinate             " << setprecision(4) << Y << endl;
+    cout << "-------------------------------\n";
+    if (coefA > 0) {cout << "The parabola opens UPWARD\n";}
+    else {cout << "The parabola opens DOWNWARD\n";}
+    cout << "-------------------------------\n";
+    
+    
+    
+    // ----------------------------
+    
+    return 0;
 }
---
+++ random/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 4;
+    int sum = 24;
+    cout << "num is " << num << endl << "sum is " << sum << endl;
+    
+    return 0;
+}
